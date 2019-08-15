d50906770ded43ac241be66c08433a911075eacf
Andreas Sujono <andreassujono@gmail.com>
2019-08-15 15:21:51 +0800

    Updates

---
+++ precache-manifest.f17ff0c9cc42330ab487c9c862fb841c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1233d48074cbe627e46c577b77d23f",
+    "url": "/ideaList/index.html"
+  },
+  {
+    "revision": "8b4bfdeebec5cbba8ac9",
+    "url": "/ideaList/static/css/main.c45b658a.chunk.css"
+  },
+  {
+    "revision": "2bd6fe0f3594ff4a925f",
+    "url": "/ideaList/static/js/2.abb9a3ad.chunk.js"
+  },
+  {
+    "revision": "8b4bfdeebec5cbba8ac9",
+    "url": "/ideaList/static/js/main.167102a7.chunk.js"
+  },
+  {
+    "revision": "68776d20ea729290e302",
+    "url": "/ideaList/static/js/runtime~main.2d5eed7d.js"
+  }
+]);

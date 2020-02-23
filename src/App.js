4ef3759db168d281abfc067b2f11f3fdf4115610
Andreas_Sujono <andreassujono@gmail.com>
2020-02-23 18:06:05 +0800

    fix initizalization

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {HashRouter as Router, Route, Switch, Link, NavLink} from 'react-router-dom'
 
-import Home from './components/mainComponent/home.js'
-import About from './components/mainComponent/about.js'
-import Login from './components/mainComponent/login.js'
-import SignUp from './components/mainComponent/signUp.js'
+import Home from 'components/mainComponent/home.js'
+import About from 'components/mainComponent/about.js'
+import Login from 'components/mainComponent/login.js'
+import SignUp from 'components/mainComponent/signUp.js'
 
-import NewsSingle from './components/mainComponent/newsSingle.js'
-import MyNews from './components/mainComponent/myNews.js'
-import MyProfile from './components/mainComponent/myProfile.js'
-import Post from './components/mainComponent/post.js'
-
-
-
-
-
+import NewsSingle from 'components/mainComponent/newsSingle.js'
+import MyNews from 'components/mainComponent/myNews.js'
+import MyProfile from 'components/mainComponent/myProfile.js'
+import Post from 'components/mainComponent/post.js'
 
 function App() {
   return (
